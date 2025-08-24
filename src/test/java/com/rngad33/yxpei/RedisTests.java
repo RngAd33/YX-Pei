a780624373272cf4f50e0bc1ca9ad6501678ab7d
RngAd33 <yxbsyx@outlook.com>
2025-08-24 15:31:06 +0800

    性能优化

--- src/test/java/com/rngad33/yxpei/RedisTests.java
+++ src/test/java/com/rngad33/yxpei/RedisTests.java
@@ -16,10 +16,46 @@ import static org.junit.jupiter.api.Assertions.*;
 class RedisTests {
 
     @Resource
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @Resource
     private StringRedisTemplate stringRedisTemplate;
 
     @Test
-    public void redisTest() {
+    public void redisTest1() {
+        // 获取redis操作对象
+        ValueOperations<String, Object> valueOps = redisTemplate.opsForValue();
+
+        // 设置模拟数据
+        String key = "testKey";
+        String value = "testValue";
+
+        // 新增、更新测试
+        valueOps.set(key, value);   // 设置键值对
+        String storedValue = (String) valueOps.get(key);
+        assertEquals(value, storedValue, "——！存储的值与预期不一致！——");
+
+        // 修改测试
+        String updateValue = "updatedValue";
+        valueOps.set(key, updateValue);
+        storedValue = (String) valueOps.get(key);
+        assertEquals(updateValue, storedValue, "——！更新后的值与预期不一致！——");
+
+        // 查询测试
+        storedValue = (String) valueOps.get(key);
+        assertNotNull(storedValue, "——！查询失败！——");
+        assertEquals(updateValue, storedValue, "——！查询的值与预期不一致！——");
+
+        // 删除测试
+        stringRedisTemplate.delete(key);
+        storedValue = (String) valueOps.get(key);
+        assertNull(storedValue, "——！删除失败！——");
+
+        System.out.println("测试结束，运行正常>>>");
+    }
+
+    @Test
+    public void redisTest2() {
         // 获取redis操作对象
         ValueOperations<String, String> valueOps = stringRedisTemplate.opsForValue();
 
