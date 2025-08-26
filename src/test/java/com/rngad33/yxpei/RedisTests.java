5c9035728da0005add5cf84b8f7ebbc4ef11b36d
RngAd33 <yxbsyx@outlook.com>
2025-08-26 17:21:46 +0800

    性能优化

--- src/test/java/com/rngad33/yxpei/RedisTests.java
+++ src/test/java/com/rngad33/yxpei/RedisTests.java
@@ -2,11 +2,14 @@ package com.rngad33.yxpei;
 
 import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
+import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -116,6 +119,26 @@ class RedisTests {
         assertNull(storedValue, "——！删除失败！——");
 
         System.out.println("测试结束，运行正常>>>");
+    }
+
+    @Test
+    void testWatchDog() {
+        RLock lock = redissonClient.getLock("yxpei:precachejob:docache:lock");
+        try {
+            // 只有一个线程能获取到锁
+            if (lock.tryLock(0, -1, TimeUnit.MILLISECONDS)) {
+                // todo 实际要执行的方法
+                System.out.println("getLock: " + Thread.currentThread().getId());
+            }
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            // 只能释放自己的锁
+            if (lock.isHeldByCurrentThread()) {
+                System.out.println("unLock: " + Thread.currentThread().getId());
+                lock.unlock();
+            }
+        }
     }
 
 }
