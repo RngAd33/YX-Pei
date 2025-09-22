9423712ec02ea668b6fbe6d389492c1b30ce12b7
RngAd33 <yxbsyx@outlook.com>
2025-09-22 11:02:04 +0800

    改造

--- src/main/java/com/rngad33/yxpei/config/RedissonConfig.java
+++ src/main/java/com/rngad33/yxpei/config/RedissonConfig.java
@@ -3,7 +3,7 @@ package com.rngad33.yxpei.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,25 +13,40 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RedissonConfig {
 
+    @Value("${spring.data.redis.database}")
+    private int database;
+
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.data.redis.password}")
+    private String password;
+
+    @Value("${spring.data.redis.timeout}")
+    private long timeout;
+
     /**
      * 初始化Redisson客户端
      *
-     * @param redisProperties
      * @return
      */
     @Bean
-    public RedissonClient redissonClient(RedisProperties redisProperties) {
-        // 创建配置
+    public RedissonClient redissonClient() {
         Config config = new Config();
-        // - 构建Redis连接地址
-        String redisAddress = "redis://" + redisProperties.getHost() + ":" + redisProperties.getPort();
-        // - 配置Redisson连接（暂不使用集群）
+        // 构建Redis连接地址
+        String redisAddress = "redis://" + redisHost + ":" + redisPort;
+        // 设置看门狗超时时间（默认30秒）
+        config.setLockWatchdogTimeout(timeout);
+        // 配置Redisson连接（暂不使用集群）
         config.useSingleServer()
                 .setAddress(redisAddress)
-                .setDatabase(redisProperties.getDatabase());
+                .setDatabase(database);
         // - 如果有密码则设置密码
-        if (redisProperties.getPassword() != null) {
-            config.useSingleServer().setPassword(redisProperties.getPassword());
+        if (password != null && !password.isEmpty()) {
+            config.useSingleServer().setPassword(password);
         }
         // 创建并返回Redisson实例
         return Redisson.create(config);
